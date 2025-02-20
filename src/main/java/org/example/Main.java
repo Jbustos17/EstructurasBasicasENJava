46dab6407b474791aa61fc643a48a779d2908d63
Jbustos17 <juandavidbustos04@gmail.com>
2025-02-20 12:34:30 -0500

    Variables

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,65 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       //Variables
+
+        byte numero1=120;
+            System.out.println("VARIABLE DE TIPO BYTE" + numero1);
+
+
+            short numero2=30400;
+
+        System.out.println("VARIABLE DE TIPO SHORT" + numero2);
+
+
+        int numero3=235;
+        System.out.println("VARIABLE DE TIPO IN" + numero3);
+
+        long numero4=5623L;
+        System.out.println("VARIABLE DE TIPO LONG" + numero4);
+
+
+        //VARIABLES DECIMALES
+
+
+        float decimal1=32.5f;
+        System.out.println("VARIABLE DE TIPO float" + decimal1);
+
+
+        double decimal2=125.6;
+        System.out.println("VARIABLE DE TIPO DOUBLE" + decimal2);
+
+
+        //VARIABLES DE TIPO CADENA DE CARACTER
+
+
+        String nombre="Juan david bustos castro";
+        System.out.println("VARIABLE DE TIPO CADENA DE CARACTER" + nombre);
+
+        //VARIABLE DE TIPO CARACTER
+
+        char letra= 'a';
+        System.out.println("VARIABLE DE TIPO CHAR" + letra);
+
+
+        //VARIABLE DE TIPO BOOLEAN
+
+
+        boolean estado=true;
+        System.out.println("VARIABLE DE TIPO BOOLEAN" + estado);
+
+
+
+
+
+
+
+
+
+
+    }
+}
